51216999bd667b32ff7ba83c8de1615c0c5d2c63
Sean Diefenderfer <sean@quickclickswebsites.com>
2026-03-20 14:48:48 -0400

    Initial commit for Rackstat Production

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBOQFeokKaNQKwaUhXMCA0oEm4W9G0qafE",
+  authDomain: "rackstat-production.firebaseapp.com",
+  projectId: "rackstat-production",
+  storageBucket: "rackstat-production.firebasestorage.app",
+  messagingSenderId: "345003825117",
+  appId: "1:345003825117:web:2ad86b1902dfdebe0c8834",
+  measurementId: "G-W421R68J54"
+};
