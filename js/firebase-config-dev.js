7d479604fda78b7ac25bdc2dc0b3dffc9f58ed80
Sean Diefenderfer <sean@quickclickswebsites.com>
2026-05-05 18:38:23 -0400

    Add role-based guides, onboarding prompts, and shared header layout

---
+++ js/firebase-config-dev.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAynmWumcxD_RT_2lH0txJqLUVwm2iBDqY",
+  authDomain: "rackstat-dev.firebaseapp.com",
+  projectId: "rackstat-dev",
+  storageBucket: "rackstat-dev.firebasestorage.app",
+  messagingSenderId: "499292558475",
+  appId: "1:499292558475:web:19bad2681ffa809771708e"
+};
+
+export const recaptchaSiteKey = "6Lej0NgsAAAAAJv-lCmisAvkZfcyNuOd3qu252D7";
